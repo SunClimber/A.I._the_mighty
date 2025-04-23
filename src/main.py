8756f7c051254dd8356b882ce3fec8386dddc1f2
Sam <sreynebeau11@gmail.com>
2025-04-23 18:22:45 -0500

    gutendex api

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import gui
 from dotenv import load_dotenv
 import os
+import requests
 
 #import random
 #import time
@@ -13,6 +14,24 @@ load_dotenv()
 import google.generativeai as genai
 
 api_key = os.getenv("API_KEY")
+
+#user input
+user_search = input("please input an author to search for a list of his books available on Project Gutenberg: ")
+
+
+response = requests.get(f"https://gutendex.com/books?search={user_search}")
+
+
+data = response.json()
+
+results = data["results"]
+
+
+
+for book in results:
+    print(book["title"])
+
+#print(data["results"][0]["title"])
 
 #set API using api-key
 genai.configure(api_key= api_key)
